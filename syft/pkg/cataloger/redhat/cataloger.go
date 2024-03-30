a088f4ae7b5439c4ea9219661f5b8fa9624513f3
计理强 <3085200340@qq.com>
2024-03-31 01:06:35 +0800

    3-30

--- syft/pkg/cataloger/redhat/cataloger.go
+++ syft/pkg/cataloger/redhat/cataloger.go
@@ -3,6 +3,10 @@ Package redhat provides a concrete DBCataloger implementation relating to packag
 */
 package redhat
 
+//导入了一些必要的库，用于数据库交互（database/sql）、
+//日志记录（github.com/anchore/syft/internal/log）、
+//软件包处理（github.com/anchore/syft/syft/pkg）以及
+//通用编目器（github.com/anchore/syft/syft/pkg/cataloger/generic
 import (
 	"database/sql"
 
@@ -11,6 +15,15 @@ import (
 	"github.com/anchore/syft/syft/pkg/cataloger/generic"
 )
 
+// NewDBCataloger 函数:
+// 此函数创建一个新的编目器，用于处理存储在数据库（可能是 RPM 数据库）中的 Red Hat 软件包。
+// 它会检查 sqlite 驱动程序是否可用（用于访问 RPM 数据库）。
+// 如果不可用，则会记录一条警告，指示可能存在与较新数据库相关的问题。
+// 它创建一个名为 “rpm-db-cataloger” 的通用编目器。
+// 它使用基于文件 glob 的两个解析器配置编目器:
+// parseRpmDB: 此函数可能解析 RPM 数据库中的条目。
+// parseRpmManifest: 此函数可能解析数据库中的包清单。
+// 两个解析器都与特定的 glob 模式相关联（pkg.RpmDBGlob 和潜在的清单模式）。
 // NewDBCataloger returns a new RPM DB cataloger object.
 func NewDBCataloger() pkg.Cataloger {
 	// check if a sqlite driver is available
@@ -23,12 +36,22 @@ func NewDBCataloger() pkg.Cataloger {
 		WithParserByGlobs(parseRpmManifest, pkg.RpmManifestGlob)
 }
 
+// NewArchiveCataloger 函数:
+// 此函数创建一个新的编目器，用于处理来自存档文件（可能是 RPM 文件）的 Red Hat 软件包。
+// 它创建一个名为 “rpm-archive-cataloger” 的通用编目器。
+// 它使用单个解析器配置编目器:
+// parseRpmArchive: 此函数可能解析来自单个 RPM 存档文件的信息。
+// 解析器与通用 glob 模式 **/*.rpm 相关联，该模式以任何子目录中的所有 .rpm 扩展名文件为目标。
 // NewArchiveCataloger returns a new RPM file cataloger object.
 func NewArchiveCataloger() pkg.Cataloger {
 	return generic.NewCataloger("rpm-archive-cataloger").
 		WithParserByGlobs(parseRpmArchive, "**/*.rpm")
 }
 
+// isSqliteDriverAvailable 函数:
+// 此辅助函数检查系统中是否可用 sqlite 数据库驱动程序。
+// 它尝试打开与内存中 SQLite 数据库的连接。
+// 如果连接成功（没有错误），则返回 true，指示驱动程序可用。
 func isSqliteDriverAvailable() bool {
 	_, err := sql.Open("sqlite", ":memory:")
 	return err == nil
--- syft/pkg/cataloger/redhat/package.go
+++ syft/pkg/cataloger/redhat/package.go
@@ -1,5 +1,6 @@
 package redhat
 
+//该代码片段定义了用于处理 Red Hat (RPM) 软件包的函数，属于 syft 项目的一部分
 import (
 	"fmt"
 	"strconv"
@@ -11,6 +12,11 @@ import (
 	"github.com/anchore/syft/syft/pkg"
 )
 
+// newDBPackage 函数:
+// 基于来自数据库 (db) 或 RPM 包文件 (dbOrRpmLocation) 的信息创建一个 pkg.Package 对象。
+// 该对象包含软件包的名称 (Name)、版本 (Version)、许可证 (Licenses)、PURL (软件包通用资源定位符)、位置 (Locations)、类型 (Type) 和元数据 (Metadata) 等信息。
+// toELVersion 函数用于将 RPM 版本转换为 Enterprise Linux (EL) 风格的格式。
+// packageURL 函数用于生成软件包的 PURL 字符串。
 func newDBPackage(dbOrRpmLocation file.Location, m pkg.RpmDBEntry, distro *linux.Release, licenses []string) pkg.Package {
 	p := pkg.Package{
 		Name:      m.Name,
@@ -26,6 +32,8 @@ func newDBPackage(dbOrRpmLocation file.Location, m pkg.RpmDBEntry, distro *linux
 	return p
 }
 
+// newArchivePackage 函数:
+// 基于来自存档文件 (archiveLocation) 的信息创建 pkg.Package 对象，过程类似于 newDBPackage 函数。
 func newArchivePackage(archiveLocation file.Location, m pkg.RpmArchive, licenses []string) pkg.Package {
 	p := pkg.Package{
 		Name:      m.Name,
@@ -41,6 +49,9 @@ func newArchivePackage(archiveLocation file.Location, m pkg.RpmArchive, licenses
 	return p
 }
 
+// newMetadataFromManifestLine 函数:
+// 解析 RPM 清单文件中的每一行，并从中提取软件包元数据 (e.g., 名称、版本、架构)。
+// 该函数专用于解析 Mariner 容器使用的特殊格式的清单文件。
 // newMetadataFromManifestLine parses an entry in an RPM manifest file as used in Mariner distroless containers.
 // Each line is the output from:
 // - rpm --query --all --query-format "%{NAME}\t%{VERSION}-%{RELEASE}\t%{INSTALLTIME}\t%{BUILDTIME}\t%{VENDOR}\t%{EPOCH}\t%{SIZE}\t%{ARCH}\t%{EPOCHNUM}\t%{SOURCERPM}\n"
@@ -83,6 +94,10 @@ func newMetadataFromManifestLine(entry string) (*pkg.RpmDBEntry, error) {
 	}, nil
 }
 
+// packageURL 函数:
+// 根据提供的软件包信息生成 PURL 字符串。
+// PURL 用于唯一标识软件包，并包含名称、版本、架构、发行版等信息。
+// 该函数考虑了发行版 (distro) 信息，可以生成特定于发行版的 PURL。
 // packageURL returns the PURL for the specific RHEL package (see https://github.com/package-url/purl-spec)
 func packageURL(name, arch string, epoch *int, srpm string, version, release string, distro *linux.Release) string {
 	var namespace string
